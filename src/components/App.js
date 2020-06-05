a20efb37ed3308087fb814aad49b4bde64c755d9
Juakata <juakata07@gmail.com>
2020-06-05 13:53:31 -0600

    initial structure

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <button type="button" className="ui button primary">
+        Posts
+      </button>
+      <button type="button" className="ui button primary">
+        Todo
+      </button>
+    </div>
+  );
+}
+
+export default App;
